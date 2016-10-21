1a0118481e3552117d08eaec79b57406c1832a9d
acoomes <86drift@gmail.com>
2016-10-20 22:35:45 -0700

    Add loadImage function, comment out #intro background property in lieu of Weather Underground API call setting background. Leave background property in place for fallback purposes.

--- portfolio/scripts/app.js
+++ portfolio/scripts/app.js
@@ -8,12 +8,14 @@ function init() {
       $('.sidebar-wrapper').toggleClass('open');
     });
     // Query Weather Underground API and set the conditions
+    getWeather();
     function getWeather() {
       $.ajax({
         url: 'http://api.wunderground.com/api/0fab5cf05915f696/geolookup/conditions/q/97601.json',
         dataType: 'jsonp',
         success: function(response) {
           var conditions = response.current_observation.weather;
+          loadImage(conditions);
         }
       });
     }
@@ -31,6 +33,22 @@ function init() {
         timeOfDay = 'morning';
       }
       return timeOfDay;
+    }
+    // Set the background image for the intro section based on the passed conditions
+    function loadImage(conditions) {
+      var imageSRC = 'img/weather/hero-';
+      var validConditions = ['clear', 'cloudy', 'rain', 'snow'];
+      var timeOfDay = getTimeOfDay();
+      var conditions = conditions.toLowerCase();
+      for (var i = 0; i < validConditions.length; i++) {
+        if (conditions === validConditions[i]) {
+          break;
+        } else if (i === validConditions.length - 1){
+          conditions = 'cloudy';
+        }
+      }
+      var url = imageSRC + conditions + '-' + timeOfDay + '.jpg';
+      $('#intro').css('background-image', 'url(' + url + ')');
     }
   });
 }
