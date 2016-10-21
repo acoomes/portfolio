b59ad3535075d33595bc864d218e4226b8302591
acoomes <86drift@gmail.com>
2016-10-20 21:38:49 -0700

    Add getWeather and getTimeOfDay functions to app.js, add weather images to img/weather directory

--- portfolio/scripts/app.js
+++ portfolio/scripts/app.js
@@ -1,4 +1,5 @@
 function init() {
+  // Sidebar functions - open and close the sidebar when hamburger or close button is clicked
   $(document).ready(function() {
     $('#close-btn').click(function() {
       $('.sidebar-wrapper').toggleClass('open');
@@ -6,6 +7,31 @@ function init() {
     $('#hamburger').click(function() {
       $('.sidebar-wrapper').toggleClass('open');
     });
+    // Query Weather Underground API and set the conditions
+    function getWeather() {
+      $.ajax({
+        url: 'http://api.wunderground.com/api/0fab5cf05915f696/geolookup/conditions/q/97601.json',
+        dataType: 'jsonp',
+        success: function(response) {
+          var conditions = response.current_observation.weather;
+        }
+      });
+    }
+    // Get the current, local time and return the categorical time of day (morning, afternoon, or night)
+    function getTimeOfDay() {
+      var date = new Date();
+      var time = date.toLocaleTimeString();
+      var hours = date.getHours();
+      var timeOfDay;
+      if (hours > 17) {
+        timeOfDay = 'night';
+      } else if (hours > 12 && hours < 16) {
+        timeOfDay = 'afternoon';
+      } else {
+        timeOfDay = 'morning';
+      }
+      return timeOfDay;
+    }
   });
 }
 init();
